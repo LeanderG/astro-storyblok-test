aeb228663a2070449b31c07f092b9831980d8d60
Leander Gilles <Leander@neugelb.com>
2023-02-09 10:55:43 +0100

    updated environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  PUBLIC_STORYBLOK_PREVIEW_TOKEN: string;
+  VITE_ASTRO_SSR: "true" | "false";
+  PUBLIC_STORYBLOK_API_VERSION: "draft" | "published";
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
